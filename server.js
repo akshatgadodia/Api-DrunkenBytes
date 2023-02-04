b175b258399e7bbf89e37f7f48943a2d85e2bd7b
akshatgadodia <akshatgadodia@gmail.com>
2023-02-04 16:13:45 +0530

    Changes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 //Enabling CORS
 const corsConfig = require('./config/cors')
 const cors = require("cors");
-app.use(cors());
+app.use(cors(corsConfig));
 //Cookie Parser
 const cookieParser = require('cookie-parser')
 app.use(cookieParser());
