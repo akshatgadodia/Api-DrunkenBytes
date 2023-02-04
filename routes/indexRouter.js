822912d127c9ed7864ad57e75a7831741068186a
akshatgadodia <akshatgadodia@gmail.com>
2023-02-04 20:13:50 +0530

    Changes Pushed

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,18 +3,18 @@ const router = express.Router()
 
 //User Route
 const userRoutes = require("./userRoute");
-router.use("/api/user", userRoutes);
+router.use("/v1/user", userRoutes);
 //Support User Route
 const supportUserRoutes = require("./supportUserRoute");
-router.use("/api/support-user", supportUserRoutes);
+router.use("/v1/support-user", supportUserRoutes);
 //NFT Route
 const nftRoutes = require("./nftRoute");
-router.use("/api/nft", nftRoutes)
+router.use("/v1/nft", nftRoutes)
 //Transaction Routes
 const transactionRoutes = require("./transactionRoute");
-router.use("/api/transaction", transactionRoutes)
+router.use("/v1/transaction", transactionRoutes)
 //Api Key Routes
 const apiKeyRoutes = require("./apiKeyRoute");
-router.use("/api/api-key", apiKeyRoutes)
+router.use("/v1/api-key", apiKeyRoutes)
 
 module.exports = router
