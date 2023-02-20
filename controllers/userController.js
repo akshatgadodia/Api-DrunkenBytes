8837f0d543f7c64ce042617de3b84fd026f41105
akshatgadodia <akshatgadodia@gmail.com>
2023-02-20 15:17:34 +0530

    Initial Login Added

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,8 +3,32 @@ const asyncHandler = require("../middlewares/asyncHandler");
 const ErrorResponse = require("../utils/errorResponse");
 var jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const { v4: uuidv4 } = require('uuid');
+const { web3 } = require("../config/web3");
+
+const initialLoginUser = asyncHandler(async (req, res, next) => {
+  const accountAddress = req.body.accountAddress;
+  const message = `
+Welcome to Drunken Bytes!\n
+Click to sign in and accept the OpenSea Terms of Service: https://drunkenbytes.vercel.app/terms-of-service\n
+This request will not trigger a blockchain transaction or cost any gas fees.\n
+Your authentication status will reset after you close the browser.\n
+Wallet address:
+${accountAddress}\n
+Nonce:
+${uuidv4()}
+  `
+  res.status(200).json({
+    success: true,
+    data: {
+      message
+    }
+  });
+});
 
 const loginUser = asyncHandler(async (req, res, next) => {
+  const recoveredAddress = await web3.eth.accounts.recover(req.body.message.toString(), req.body.signedData.toString());
+  if (recoveredAddress !== req.body.accountAddress) return next(new ErrorResponse("Invalid User", 403));;
   let user = await User.findOne({ accountAddress: req.body.accountAddress });
   if (!user) {
     const data = {
@@ -28,7 +52,7 @@ const loginUser = asyncHandler(async (req, res, next) => {
     process.env.D_B_SECRET_KEY,
     { expiresIn: "7d" }
   );
-  res.cookie("userAccessToken", accessToken,{
+  res.cookie("userAccessToken", accessToken, {
     // expires: new Date(Date.now() + ( 7 * 24 * 60 * 60 * 1000)),
     secure: true, // set to true if your using https or samesite is none
     sameSite: 'none', // set to none for cross-request
@@ -56,7 +80,7 @@ const updateUserData = asyncHandler(async (req, res, next) => {
   console.log(user)
   res.status(200).json({
     success: true,
-    data: {message: "User Data Successfully updated"}
+    data: { message: "User Data Successfully updated" }
   });
 });
 
@@ -96,4 +120,4 @@ const getAllUsers = asyncHandler(async (req, res, next) => {
     }
   });
 });
-module.exports = { loginUser, updateUserData, getUser, getAllUsers, logoutUser };
+module.exports = { loginUser, updateUserData, getUser, getAllUsers, logoutUser, initialLoginUser };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,10 +3,11 @@ const router = express.Router()
 const verifyUserToken = require("../middlewares/verifyUserToken")
 const verifyUserRoles = require("../middlewares/verifyUserRoles")
 const ROLES_LIST = require("../utils/rolesList")
-const { loginUser, updateUserData, getUser, getAllUsers, logoutUser } = require('../controllers/userController')
+const { loginUser, updateUserData, getUser, getAllUsers, logoutUser, initialLoginUser } = require('../controllers/userController')
 
 router.get("/get-user", getUser);
 router.get("/get-all-users", getAllUsers);
+router.post('/initial-login', initialLoginUser);
 router.post('/login', loginUser);
 router.patch('/update', verifyUserToken, verifyUserRoles(ROLES_LIST.ADMIN, ROLES_LIST.USER), updateUserData);
 router.post("/logout", logoutUser)
