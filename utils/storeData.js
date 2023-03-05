ff67f16bc7df7af9d3708e5a4f5a68388267ac82
akshatgadodia <akshatgadodia@gmail.com>
2023-03-05 23:44:06 +0530

    Small Error Resolved

--- utils/storeData.js
+++ utils/storeData.js
@@ -11,7 +11,7 @@ const saveDataOnIPFS = async values => {
     const NFTData = {
       description: "A warranty card NFT is a unique digital asset that represents a warranty for a physical product or service. It is created and stored on a blockchain network, which ensures its authenticity and traceability. The owner of the warranty card NFT can use it to claim their warranty in case of any defects or issues with the product. The NFT can also be traded or sold on various NFT marketplaces, providing a new level of flexibility and liquidity to the traditional warranty process. Overall, the warranty card NFT is a revolutionary concept that combines the benefits of blockchain technology with traditional warranties, providing an enhanced user experience for both buyers and sellers.",
       image: "ipfs://QmWvHbUYRZEUr4hx617nBiCsH2zrAGMBbLxJZEUpzwa6hh",
-      external_url: `https://claims.drunkenbytes.vercel.app/${tokenId}`, 
+      external_url: `https://claims.drunkenbytes.vercel.app/${values.tokenId}`, 
       name: "Warranty Card",
       attributes: [
         { trait_type: "Token ID", value: values.tokenId.toString() },
